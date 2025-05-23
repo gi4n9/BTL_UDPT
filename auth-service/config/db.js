a6bd60ec6cbb0c9eea5b5d195d403752989a2485
gi4n9 <giangnguyenlamtruong@gmail.com>
2025-05-23 09:40:42 +0700

    update base

--- auth-service/config/db.js
+++ auth-service/config/db.js
@@ -1,6 +1,25 @@
-const knex = require("knex");
-const config = require("./knex-file");
+require("dotenv").config();
+const { MongoClient } = require("mongodb");
 
-let db = knex(config.development);
+const url = process.env.MONGO_URL;
+const dbName = process.env.DB_NAME;
 
-module.exports = db;
+module.exports.connectDB = async function connectDB() {
+  const client = new MongoClient(url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+
+  try {
+    // Kết nối tới MongoDB
+    await client.connect();
+    console.log("Kết nối thành công tới MongoDB!");
+
+    // Chọn database
+    const db = client.db(dbName);
+    return db; // Trả về đối tượng database để sử dụng
+  } catch (err) {
+    console.error("Lỗi kết nối:", err);
+    throw err;
+  }
+};
--- auth-service/config/validate-schema.js
+++
@@ -1,8 +0,0 @@
-const Joi = require("joi");
-module.exports.registerBody = Joi.object({
-  email: Joi.string().email({
-    minDomainSegments: 2,
-    tlds: { allow: ["com", "net"] },
-  }),
-  password: Joi.string().pattern(new RegExp("^[a-zA-Z0-9]{3,30}$")),
-});
