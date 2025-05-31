bad2ff48b7999b606b13fcf5f1599ca6ab917c1a
gi4n9 <giangnguyenlamtruong@gmail.com>
2025-05-31 11:24:40 +0700

    connect express

--- auth-service/config/db.js
+++ auth-service/config/db.js
@@ -4,7 +4,7 @@ require("dotenv").config();
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI);
-    console.log("MongoDB connected successfully");
+    console.log("MongoDB connected successfully to auth_db");
   } catch (error) {
     console.error("MongoDB connection error:", error);
     process.exit(1);
