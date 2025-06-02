0eb62351b3c7f8cb33a58917587fce689300e4f0
gi4n9 <giangnguyenlamtruong@gmail.com>
2025-06-02 21:15:40 +0700

    ...

--- user-service/src/middlewares/auth.js
+++ user-service/src/middlewares/auth.js
@@ -1,37 +1,39 @@
 const jwt = require("jsonwebtoken");
 
 const auth = (req, res, next) => {
-    const token = req.headers.authorization?.split(" ")[1];
-    console.log(token);
-    if (!token) {
-        return res.status(401).json({ message: "Unauthorized" });
-    }
-    next();
-}
+  const token = req.headers.authorization?.split(" ")[1];
+  console.log("token:", token);
+  if (!token) {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
+  next();
+};
 
 const checkRole = (role) => {
-    return (req, res, next) => {
-        const token = req.headers.authorization?.split(" ")[1];
-        if (!token) return res.status(401).json({ message: "No token provided" });
+  return (req, res, next) => {
+    const token = req.headers.authorization?.split(" ")[1];
+    console.log("token to check role:", token);
+    if (!token) return res.status(401).json({ message: "No token provided" });
 
-        try {
-            const decoded = jwt.verify(token, process.env.JWT_SECRET);
-            console.log(decoded);
-            
-            req.userId = decoded.userId;
-            req.email = decoded.email;
-            req.status = decoded.status;
-            req.role = decoded.role;
+    try {
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+      console.log("decode:", decoded);
 
-            if (req.role !== role) {
-                return res.status(401).json({ message: "Unauthorized" });
-            }
+      req.userId = decoded.userId;
+      req.email = decoded.email;
+      req.status = decoded.status;
+      req.role = decoded.role;
 
-            next();
-        } catch (error) {
-            res.status(401).json({ message: "Invalid token" });
-        }
+      if (req.role !== role) {
+        return res.status(401).json({ message: "Unauthorized" });
+      }
+
+      next();
+    } catch (error) {
+      console.log("error:", error);
+      res.status(401).json({ message: "Invalid token" });
     }
-}
+  };
+};
 
 module.exports = { auth, checkRole };
